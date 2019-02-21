9ac65307d9295d6b72f14b6acf96068c0bf3a683
Helber Maciel Guerra <helber.guerra@ahgora.com.br>
2019-02-21 11:40:43 -0300

    Initial commit

---
+++ command/connect.go
@@ -0,0 +1,60 @@
+package command
+
+import (
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/coreos/etcd/client"
+	"github.com/coreos/etcd/pkg/transport"
+	"github.com/urfave/cli"
+	"golang.org/x/net/context"
+)
+
+func contextWithCommandTimeout(c *cli.Context) (context.Context, context.CancelFunc) {
+	return context.WithTimeout(context.Background(), c.GlobalDuration("command-timeout"))
+}
+
+func newTransport(e Etcdtool) *http.Transport {
+	tls := transport.TLSInfo{
+		CAFile:   e.CA,
+		CertFile: e.Cert,
+		KeyFile:  e.Key,
+	}
+
+	timeout := 30 * time.Second
+	tr, err := transport.NewTransport(tls, timeout)
+	if err != nil {
+		fatal(err.Error())
+	}
+
+	return tr
+}
+
+func newClient(e Etcdtool) client.Client {
+	cfg := client.Config{
+		Transport:               newTransport(e),
+		Endpoints:               strings.Split(e.Peers, ","),
+		HeaderTimeoutPerRequest: e.Timeout,
+	}
+
+	if e.Password != "" {
+		cfg.Password = e.Password
+	}
+
+	if e.User != "" {
+		cfg.Username = e.User
+		info(cfg.Username)
+	}
+
+	cl, err := client.New(cfg)
+	if err != nil {
+		fatal(err.Error())
+	}
+
+	return cl
+}
+
+func newKeyAPI(e Etcdtool) client.KeysAPI {
+	return client.NewKeysAPI(newClient(e))
+}
